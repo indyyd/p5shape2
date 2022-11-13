ea5914f88faeab2c7775af011408ce826d721926
Indy <116882977+indyyd@users.noreply.github.com>
2022-11-13 15:01:58 +0000

    Add files via upload
    1st part of the pattern

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,86 @@
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(400, 400);
     canvas.parent("p5container");
 }
 
 function draw() {
     background(220);
 
+//sweet wrapper L    
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(35, 20);
+    vertex(65, 50);
+    vertex(35, 80);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(35, 30);
+    vertex(65, 50);
+    vertex(35, 70);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(35, 40);
+    vertex(65, 50);
+    vertex(35, 60);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(35, 50);
+    vertex(65, 50);
+    vertex(35, 50);
+    endShape();
+
+    //sweet wrapper R
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(175, 20);
+    vertex(145, 50);
+    vertex(175, 80);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(175, 30);
+    vertex(145, 50);
+    vertex(175, 70);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(175, 40);
+    vertex(145, 50);
+    vertex(175, 60);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(175, 50);
+    vertex(145, 50);
+    vertex(175, 50);
+    endShape();
+
+    //body
     fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    ellipse(105, 50, 110, 55);
 }
