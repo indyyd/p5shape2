5fc4f472f70ce433bee2d46875db43813ac6da9d
Indy <116882977+indyyd@users.noreply.github.com>
2022-11-16 22:31:11 +0000

    Add files via upload
    finished pattern with no colour

--- js/sketch.js
+++ js/sketch.js
@@ -6,6 +6,172 @@ function setup() {
 function draw() {
     background(220);
 
+//top-top left
+//sweet wrapper R
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(70, -30);
+    vertex(40, 0);
+    vertex(70, 30);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(70, -20);
+    vertex(40, 0);
+    vertex(70, 20);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(70, -10);
+    vertex(40, 0);
+    vertex(70, 10);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(70, 0);
+    vertex(40, 0);
+    vertex(70, 0);
+    endShape();
+
+//body
+    fill(255, 255, 255);
+    ellipse(0, 0, 110, 55);
+
+
+//top-top mid
+//sweet wrapper L    
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(130, -30);
+    vertex(160, 0);
+    vertex(130, 30);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(130, -20);
+    vertex(160, 0);
+    vertex(130, 20);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(130, -10);
+    vertex(160, 0);
+    vertex(130, 10);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(130, 0);
+    vertex(160, 0);
+    vertex(130, 0);
+    endShape();
+
+//sweet wrapper R
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(270, -30);
+    vertex(240, 0);
+    vertex(270, 30);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(270, -20);
+    vertex(240, 0);
+    vertex(270, 20);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(270, -10);
+    vertex(240, 0);
+    vertex(270, 10);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(270, 0);
+    vertex(240, 0);
+    vertex(270, 0);
+    endShape();
+
+//body
+    fill(255, 255, 255);
+    ellipse(200, 0, 110, 55);
+
+
+//top-top R
+//sweet wrapper L    
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(330, -30);
+    vertex(360, 0);
+    vertex(330, 30);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(330, -20);
+    vertex(360, 0);
+    vertex(330, 20);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(330, -10);
+    vertex(360, 0);
+    vertex(330, 10);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(330, 0);
+    vertex(360, 0);
+    vertex(330, 0);
+    endShape();
+
+//body
+    fill(255, 255, 255);
+    ellipse(400, 0, 110, 55);
+
+
 //top L
 //sweet wrapper L    
     noFill();
@@ -86,7 +252,130 @@ function draw() {
     ellipse(100, 100, 110, 55);
 
 
-//mid mid
+//top R
+//sweet wrapper L    
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(230, 70);
+    vertex(260, 100);
+    vertex(230, 130);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(230, 80);
+    vertex(260, 100);
+    vertex(230, 120);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(230, 90);
+    vertex(260, 100);
+    vertex(230, 110);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(230, 100);
+    vertex(260, 100);
+    vertex(230, 100);
+    endShape();
+
+//sweet wrapper R
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(370, 70);
+    vertex(340, 100);
+    vertex(370, 130);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(370, 80);
+    vertex(340, 100);
+    vertex(370, 120);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(370, 90);
+    vertex(340, 100);
+    vertex(370, 110);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(370, 100);
+    vertex(340, 100);
+    vertex(370, 100);
+    endShape();
+
+//body
+fill(255, 255, 255);
+ellipse(300, 100, 110, 55);
+
+
+//mid-mid left
+//sweet wrapper R
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(70, 170);
+    vertex(40, 200);
+    vertex(70, 230);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(70, 180);
+    vertex(40, 200);
+    vertex(70, 220);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(70, 190);
+    vertex(40, 200);
+    vertex(70, 210);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(70, 200);
+    vertex(40, 200);
+    vertex(70, 200);
+    endShape();
+
+//body
+    fill(255, 255, 255);
+    ellipse(0, 200, 110, 55);
+
+
+//mid-mid mid
 //sweet wrapper L    
     noFill();
     strokeWeight(10.0);
@@ -166,84 +455,47 @@ function draw() {
     ellipse(200, 200, 110, 55);
 
 
-//top R
+//mid-mid R
 //sweet wrapper L    
     noFill();
     strokeWeight(10.0);
     strokeJoin(ROUND);
     beginShape();
-    vertex(230, 70);
-    vertex(260, 100);
-    vertex(230, 130);
+    vertex(330, 170);
+    vertex(360, 200);
+    vertex(330, 230);
     endShape();
 
     noFill();
     strokeWeight(10.0);
     strokeJoin(ROUND);
     beginShape();
-    vertex(230, 80);
-    vertex(260, 100);
-    vertex(230, 120);
+    vertex(330, 180);
+    vertex(360, 200);
+    vertex(330, 220);
     endShape();
 
     noFill();
     strokeWeight(10.0);
     strokeJoin(ROUND);
     beginShape();
-    vertex(230, 90);
-    vertex(260, 100);
-    vertex(230, 110);
+    vertex(330, 190);
+    vertex(360, 200);
+    vertex(330, 210);
     endShape();
 
     noFill();
     strokeWeight(10.0);
     strokeJoin(ROUND);
     beginShape();
-    vertex(230, 100);
-    vertex(260, 100);
-    vertex(230, 100);
-    endShape();
-
-//sweet wrapper R
-    noFill();
-    strokeWeight(10.0);
-    strokeJoin(ROUND);
-    beginShape();
-    vertex(370, 70);
-    vertex(340, 100);
-    vertex(370, 130);
-    endShape();
-
-    noFill();
-    strokeWeight(10.0);
-    strokeJoin(ROUND);
-    beginShape();
-    vertex(370, 80);
-    vertex(340, 100);
-    vertex(370, 120);
-    endShape();
-
-    noFill();
-    strokeWeight(10.0);
-    strokeJoin(ROUND);
-    beginShape();
-    vertex(370, 90);
-    vertex(340, 100);
-    vertex(370, 110);
-    endShape();
-
-    noFill();
-    strokeWeight(10.0);
-    strokeJoin(ROUND);
-    beginShape();
-    vertex(370, 100);
-    vertex(340, 100);
-    vertex(370, 100);
+    vertex(330, 200);
+    vertex(360, 200);
+    vertex(330, 200);
     endShape();
 
 //body
     fill(255, 255, 255);
-    ellipse(300, 100, 110, 55);
+    ellipse(400, 200, 110, 55);
 
 
 //bottom L
@@ -406,4 +658,168 @@ function draw() {
     ellipse(300, 300, 110, 55);
 
 
+//bott-bott left
+//sweet wrapper R
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(70, 370);
+    vertex(40, 400);
+    vertex(70, 430);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(70, 380);
+    vertex(40, 400);
+    vertex(70, 420);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(70, 390);
+    vertex(40, 400);
+    vertex(70, 410);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(70, 400);
+    vertex(40, 400);
+    vertex(70, 400);
+    endShape();
+
+//body
+    fill(255, 255, 255);
+    ellipse(0, 400, 110, 55);
+
+
+//mid-mid mid
+//sweet wrapper L    
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(130, 370);
+    vertex(160, 400);
+    vertex(130, 430);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(130, 380);
+    vertex(160, 400);
+    vertex(130, 420);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(130, 390);
+    vertex(160, 400);
+    vertex(130, 410);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(130, 400);
+    vertex(160, 400);
+    vertex(130, 400);
+    endShape();
+
+//sweet wrapper R
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(270, 370);
+    vertex(240, 400);
+    vertex(270, 430);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(270, 380);
+    vertex(240, 400);
+    vertex(270, 420);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(270, 390);
+    vertex(240, 400);
+    vertex(270, 410);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(270, 400);
+    vertex(240, 400);
+    vertex(270, 400);
+    endShape();
+
+//body
+    fill(255, 255, 255);
+    ellipse(200, 400, 110, 55);
+
+
+//mid-mid R
+//sweet wrapper L    
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(330, 370);
+    vertex(360, 400);
+    vertex(330, 430);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(330, 380);
+    vertex(360, 400);
+    vertex(330, 420);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(330, 390);
+    vertex(360, 400);
+    vertex(330, 410);
+    endShape();
+
+    noFill();
+    strokeWeight(10.0);
+    strokeJoin(ROUND);
+    beginShape();
+    vertex(330, 400);
+    vertex(360, 400);
+    vertex(330, 400);
+    endShape();
+
+//body
+    fill(255, 255, 255);
+    ellipse(400, 400, 110, 55);
 }
